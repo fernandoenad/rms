351921b027146084faa4620c57f6e6fb9d507920
Fernando Enad <31582634+fernandoenad@users.noreply.github.com>
2025-04-28 12:02:07 +0800

    update

---
+++ resources/views/admin/applications/list/footer.blade.php
@@ -0,0 +1,37 @@
+<table border="0">
+            <tr>
+                <td width="22%" align="left">Prepared by the Division Ranking Committee
+                    <br><em>(All members should affix signature)</em>
+                    <br>
+                    <br>
+                </td>
+                <td width="18%"></td>
+                <td width="28%"></td>
+                <td width="4%"></td>
+                <td width="28%">Approved by:</td>
+                        
+            </tr>
+            <tr align="center">
+                <td><strong>FERNANDO B. ENAD PhD</strong><br>Master Teacher II<br>HRMPSB Secretariat Head</td>
+                <td><strong>JUDITH S. APALE</strong><br>Administrative Officer IV<br>HRMPSB Member</td>
+                <td><strong>FERMIN M. ALBUTRA MPA</strong><br>Administrative Officer V<br>HRMPSB Member</td>
+                <td>&nbsp;</td>
+                <td rowspan="4">
+                    
+                    <strong>FAY C. LUAREZ EdD, PhD-TM, CESO VI</strong><br>Asst. Schools Division Superintendent<br>OIC, Office of the Schools Division Superintendent
+                </td> 
+            </tr>
+            <tr align="center">
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr align="center">
+                <td>&nbsp;</td>
+                <td><strong>RENATO D. CALAMBA PhD</strong><br>President, PSDSA<br>HRMPSB Member</td>
+                <td><strong>EDUARDO A. OMPAD EdD, CESE</strong><br>Asst. Schools Division Superintendent<br>HRMPSB Chairman</td>
+                <td>&nbsp;</td>
+
+            </tr>
+        </table>
